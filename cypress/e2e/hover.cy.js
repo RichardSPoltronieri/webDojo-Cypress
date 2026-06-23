786a3e8ccac98bbfe870d6128af88799d5af20b0
RichardSPoltronieri <ri_soares@yahoo.com.br>
2026-06-23 19:24:18 -0300

    Simulando eventos com Mouseover

---
+++ cypress/e2e/hover.cy.js
@@ -0,0 +1,11 @@
+describe('Simulando mouse hover', () => {
+    it('Deve mostrar um texto ao passar o mouse em cima', () => {
+        cy.start()
+        cy.submitLoginForm('papito@webdojo.com','katana123')
+
+        cy.contains('Isso é Mouseover!').should('not.exist')
+        cy.get('[data-cy="instagram-link"]').realHover()
+        cy.contains('Isso é Mouseover!').should('exist')
+
+    })
+})

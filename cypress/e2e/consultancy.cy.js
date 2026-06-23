b1ea7cd57be38781135073a4203a3d507ca5a967
RichardSPoltronieri <ri_soares@yahoo.com.br>
2026-06-22 21:36:32 -0300

    Implementação de verificação de campos obrigatórios

--- cypress/e2e/consultancy.cy.js
+++ cypress/e2e/consultancy.cy.js
@@ -1,5 +1,5 @@
 describe('Formulário de consultoria', () => {
-    it('Deve solicitar consultoria individual', () => {
+    it.only('Deve solicitar consultoria individual', () => {
         cy.start()
         cy.submitLoginForm('papito@webdojo.com', 'katana123')
 
@@ -92,5 +92,40 @@ describe('Formulário de consultoria', () => {
         cy.contains('Sua solicitação de consultoria foi enviada com sucesso! Em breve, nossa equipe entrará em contato através do email fornecido.')
             .should('be.visible')
     })
+
+    it('Deve verificar os campos obrigatórios', () => {
+        cy.start()
+        cy.submitLoginForm('papito@webdojo.com', 'katana123')
+        cy.goTo('Formulários', 'Consultoria')
+        cy.contains('button', 'Enviar formulário')
+            .click()
+
+        cy.contains('label', 'Nome Completo')
+            .parent()
+            .find('p')
+            .should('be.visible')
+            .should('have.text', 'Campo obrigatório')
+            .and('have.class', 'text-red-400')
+            .and('have.css', 'color', 'rgb(248, 113, 113)')
+
+        cy.contains('label', 'Email')
+            .parent()
+            .find('p')
+            .should('be.visible')
+            .should('have.text', 'Campo obrigatório')
+            .and('have.class', 'text-red-400')
+            .and('have.css', 'color', 'rgb(248, 113, 113)')
+
+        cy.contains('label', 'termos de uso')
+            .parent()
+            .find('p')
+            .should('be.visible')
+            .should('have.text', 'Você precisa aceitar os termos de uso')
+            .and('have.class', 'text-red-400')
+            .and('have.css', 'color', 'rgb(248, 113, 113)')
+
+    })
+
+
 })
 

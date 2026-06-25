b163b27d59d4b2f99ec53acd5fdb7473b7ae2f40
RichardSPoltronieri <tecnicorichard1@gmail.com>
2026-06-24 21:59:25 -0300

    Implementação de custom Commands

--- cypress/e2e/super.consultancy.cy.js
+++ cypress/e2e/super.consultancy.cy.js
@@ -8,169 +8,19 @@ describe('Formulário de consultoria', () => {
     });
 
     it('Deve solicitar consultoria individual', () => {
-
-        cy.get('input[placeholder="Digite seu nome completo"]').type(personal.name)
-        cy.get('input[placeholder="Digite seu email"]').type(personal.email)
-        cy.get('input[placeholder="(00) 00000-0000"]')
-            .type(personal.phone)
-
-        cy.contains('label', 'Tipo de Consultoria')
-            .parent()
-            .find('select')
-            .select(personal.consultancyType)
-
-        if (personal.personType === 'cpf') {
-            cy.contains('label', 'Pessoa Física')
-                .find('input')
-                .click()
-                .should('be.checked')
-
-            cy.contains('label', 'Pessoa Jurídica')
-                .find('input')
-                .should('be.not.checked')
-        }
-
-        if (personal.personType === 'cnpj') {
-            cy.contains('label', 'Pessoa Jurídica')
-                .find('input')
-                .click()
-                .should('be.checked')
-
-            cy.contains('label', 'Pessoa Física')
-                .find('input')
-                .should('be.not.checked')
-        }
-
-        cy.contains('label', 'CPF')
-            .parent()
-            .find('input')
-            .type(personal.document)
-
-        personal.discoveryChannels.forEach((channel) => {
-            cy.contains('label', channel)
-                .find('input')
-                .check()
-                .should('be.checked')
-        })
-
-        cy.get('input[type="file"]') // fazendo upload de arquivo
-            .selectFile(personal.file, { force: true }) //caminho relativo e o force serve pq o botão está hidden
-
-        cy.get('textarea[placeholder="Descreva mais detalhes sobre sua necessidade"]')
-            .type(personal.description)
-
-        personal.techs.forEach((tech) => {
-            cy.get('input[placeholder="Digite uma tecnologia e pressione Enter"]')
-                .type(tech)
-                .type('{enter}')
-
-            cy.contains('label', 'Tecnologias')
-                .parent()
-                .contains('span', tech)
-                .should('be.visible')
-        })
-
-        if (personal.terms === true) {
-            cy.contains('label', 'termos de uso')
-                .find('input')
-                .check()
-        }
-
-
-        cy.contains('button', 'Enviar formulário')
-            .click()
-
-
-        cy.get('.modal', { timeout: 7000 })
-            .should('be.visible')
-            .find('.modal-content')
-            .should('be.visible')
-            .and('have.text', 'Sua solicitação de consultoria foi enviada com sucesso! Em breve, nossa equipe entrará em contato através do email fornecido.')
+        cy.fillConsultancyForm(personal)
+        cy.submitConsultancyForm()
+        cy.validateConsultancyModal()
     })
 
     it('Deve solicitar consultoria in Company', () => {
-
-        cy.get('input[placeholder="Digite seu nome completo"]').type(company.name)
-        cy.get('input[placeholder="Digite seu email"]').type(company.email)
-        cy.get('input[placeholder="(00) 00000-0000"]')
-            .type(company.phone)
-
-        cy.contains('label', 'Tipo de Consultoria')
-            .parent()
-            .find('select')
-            .select(company.consultancyType)
-
-        if (company.personType === 'cpf') {
-            cy.contains('label', 'Pessoa Física')
-                .find('input')
-                .click()
-                .should('be.checked')
-
-            cy.contains('label', 'Pessoa Jurídica')
-                .find('input')
-                .should('be.not.checked')
-        }
-
-        if (company.personType === 'cnpj') {
-            cy.contains('label', 'Pessoa Jurídica')
-                .find('input')
-                .click()
-                .should('be.checked')
-
-            cy.contains('label', 'Pessoa Física')
-                .find('input')
-                .should('be.not.checked')
-        }
-
-        cy.contains('label', 'CNPJ')
-            .parent()
-            .find('input')
-            .type(company.document)
-
-        company.discoveryChannels.forEach((channel) => {
-            cy.contains('label', channel)
-                .find('input')
-                .check()
-                .should('be.checked')
-        })
-
-        cy.get('input[type="file"]') // fazendo upload de arquivo
-            .selectFile(company.file, { force: true }) //caminho relativo e o force serve pq o botão está hidden
-
-        cy.get('textarea[placeholder="Descreva mais detalhes sobre sua necessidade"]')
-            .type(company.description)
-
-        company.techs.forEach((tech) => {
-            cy.get('input[placeholder="Digite uma tecnologia e pressione Enter"]')
-                .type(tech)
-                .type('{enter}')
-
-            cy.contains('label', 'Tecnologias')
-                .parent()
-                .contains('span', tech)
-                .should('be.visible')
-        })
-
-        if (company.terms === true) {
-            cy.contains('label', 'termos de uso')
-                .find('input')
-                .check()
-        }
-
-        cy.contains('button', 'Enviar formulário')
-            .click()
-
-        cy.get('.modal', { timeout: 7000 })
-            .should('be.visible')
-            .find('.modal-content')
-            .should('be.visible')
-            .and('have.text', 'Sua solicitação de consultoria foi enviada com sucesso! Em breve, nossa equipe entrará em contato através do email fornecido.')
+        cy.fillConsultancyForm(company)
+        cy.submitConsultancyForm()
+        cy.validateConsultancyModal()
     })
 
     it('Deve verificar os campos obrigatórios', () => {
-        cy.contains('button', 'Enviar formulário')
-            .click()
-
+        cy.submitConsultancyForm()
         cy.contains('label', 'Nome Completo')
             .parent()
             .find('p')

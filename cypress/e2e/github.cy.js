b0a83760ec2e36d2fd5e8561f6d3d32219f94769
RichardSPoltronieri <tecnicorichard1@gmail.com>
2026-06-25 21:41:41 -0300

    Removendo CI e integração em janelas externas

--- cypress/e2e/github.cy.js
+++ cypress/e2e/github.cy.js
@@ -32,7 +32,7 @@ describe('Gerenciamento de perfis no Github', () => {
             .should('be.visible')
     })
 
-    it.only('Deve remover um perfil do gitHub', () => {
+    it('Deve remover um perfil do gitHub', () => {
 
         const profile = {
 
@@ -55,6 +55,31 @@ describe('Gerenciamento de perfis no Github', () => {
         
         cy.contains('table tbody', profile.username)
             .should('not.exist')
+            
+    })
+
+    it.only('Deve validar o link do gitHub', () => {
+
+        const profile = {
+
+            name: "Richard Teste",
+            username: "RichardSPoltronieri",
+            desc: "QA"
+        }
+
+        cy.get('#name').type(profile.name)
+        cy.get('#username').type(profile.username)
+        cy.get('#profile').type(profile.desc)
+
+        cy.contains('button', 'Adicionar Perfil').click()
+
+        cy.contains('table tbody tr', profile.username)
+            .should('be.visible')
+            .as('trProfile')
+
+        cy.get('@trProfile').find('a')
+            .should('have.attr', 'href', 'https://github.com/' + profile.username)
+            .and('have.attr', 'target', '_blank')
             
     })
 

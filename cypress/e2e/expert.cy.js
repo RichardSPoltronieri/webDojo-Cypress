3baceed8e7abbc035be3766338c5ced6285f2a44
RichardSPoltronieri <tecnicorichard1@gmail.com>
2026-07-21 18:13:08 -0300

    Incluindo toast

--- cypress/e2e/expert.cy.js
+++ cypress/e2e/expert.cy.js
@@ -20,4 +20,22 @@ describe('Expert', () => {
             .invoke('show')
             .should('be.visible')
     })
+
+    it.only('Não deve logar com senha incorreta', () => {
+        cy.submitLoginForm('papito@webdojo.com', 'katana321')
+
+        cy.get('[data-sonner-toaster=true]')
+            .should('be.visible')
+            .as('toast')
+
+        cy.get('@toast')
+            .find('.title')
+            .should('have.text', 'Acesso negado! Tente novamente.')
+
+        cy.wait(5000)
+
+        cy.get('@toast')
+            .should('not.exist')
+    })
+
 })
